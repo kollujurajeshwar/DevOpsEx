d11b8122a672abf6b54a8669052caf7a061ac501
kollujurajeshwar <kollujurajeshwar@gmail.com>
2021-05-08 12:58:45 +0530

    Commit -m "Intial commit"

---
+++ SampleApp01/src/test/java/com/raj/SampleApp01/AppTest.java
@@ -0,0 +1,30 @@
+package com.raj.SampleApp01;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest  {
+
+	@Test
+	public void test1() {
+		Assert.fail();
+	}
+
+	@Test
+	public void test2() {
+		Assert.fail();
+	}
+
+	@Test
+	public void test3() {
+	}
+
+	@Test
+	public void test4() {
+	}
+
+}
